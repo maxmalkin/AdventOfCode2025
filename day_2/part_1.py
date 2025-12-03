2df66b9304fff32aca2f3b33692c9c5b5a0f5273
Max Malkin <maxim_malkin@outlook.com>
2025-12-02 18:00:27 -0700

    day 2 part 1 python

---
+++ day_2/part_1.py
@@ -0,0 +1,35 @@
+def is_invalid_id(n: int) -> bool:
+    s = str(n)
+    length = len(s)
+
+    if length % 2 != 0:
+        return False
+
+    if s[0] == "0":
+        return False
+
+    mid = length // 2
+    return s[:mid] == s[mid:]
+
+
+def solve(input: str) -> int:
+    ranges = []
+    for range_str in input.strip().split(","):
+        range_str = range_str.strip()
+        if range_str:
+            start, end = map(int, range_str.split("-"))
+            ranges.append((start, end))
+
+    sum = 0
+
+    for start, end in ranges:
+        for i in range(start, end + 1):
+            if is_invalid_id(i):
+                sum += i
+
+    return sum
+
+
+if __name__ == "__main__":
+    result = solve(" ")
+    print(result)
