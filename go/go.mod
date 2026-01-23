55a7a812bbbed0e9508c1effd45a82ad7c862d87
Max Malkin <maxim_malkin@outlook.com>
2026-01-22 20:21:57 -0700

    run.sh

--- go/go.mod
+++
@@ -1,3 +0,0 @@
-module advent_of_code_2025
-
-go 1.21
--- run.sh
+++ run.sh
@@ -26,19 +26,6 @@ run_rust() {
     cd ..
 }
 
-run_go() {
-    cd go
-    for day in {1..11}; do
-        echo "Day $day Part 1:"
-        go run day_$day/part_1.go
-        echo "Day $day Part 2:"
-        go run day_$day/part_2.go
-    done
-    echo "Day 12 Part 1:"
-    go run day_12/part_1.go
-    cd ..
-}
-
 run_java() {
     cd java
     for day in {1..11}; do
